39cecbf86f1f7686ec7b5874ca2afc664baf9752
Saeed <saeed144.73@gmail.com>
2024-05-14 11:37:59 +0330

    🧾 Create first row of bid form

--- components/components.py
+++ components/components.py
@@ -12,7 +12,13 @@ class Components:
         """
         pass
 
-    def navbar(self):
+    def navbar(self) -> html.Div:
+        """
+        Create the main navbar.
+
+        :return: the main navbar component
+        :rtype: html.Div
+        """
         navbar = html.Div(
             [
                 dcc.Location(id='url', refresh=False),
@@ -31,3 +37,101 @@ class Components:
             ]
         )
         return navbar
+
+    def page_title(self, title: str, bootstrap="mt-5 text-center") -> html.H1:
+        """
+        Create the page title.
+
+        :param title: the title of the page
+        :param bootstrap: the bootstrap class
+        :return: the page title component
+        """
+        cmp = html.H1(title, className=bootstrap)
+        return cmp
+
+    def lable(self, title:str, for_input:str, bootstrap="mb-1") -> dbc.Label:
+        """
+        Create form lable.
+
+        :param title: the title of the lable
+        :type title: str
+        :param for_input: the id of the input
+        :type for_input: str
+        :param bootstrap: the bootstrap class
+        :type bootstrap: str
+        :return: the lable component
+        :rtype: dbc.Lable
+        """
+        cmp = dbc.Label(title, html_for=for_input, className=bootstrap)
+        return cmp
+
+    def input_text(
+            self,
+            id: str,
+            placeholder="",
+            bootstrap="form-control mb-3",
+            is_diable=False
+        ) -> dcc.Input:
+        """
+        Create text input.
+
+        :param id: the id of the input
+        :type id: str
+        :param placeholder: the placeholder of the input
+        :type placeholder: str, defualt to ""
+        :param bootstrap: the bootstrap class
+        :type bootstrap: str, defualt to form-control mb-3
+        :param is_diable: if the input is going to be disable or not
+        :type is_diable: bool, defualt to False
+        :return: the text input component
+        :rtype: dcc.Input
+        """
+        cmp = dcc.Input(
+            id=id,
+            type="text",
+            placeholder=placeholder,
+            disabled=is_diable,
+            className=bootstrap
+        )
+        return cmp
+
+    def add_bid_form(self):
+        """
+        Create the add bid form.
+
+        :param: None
+        :return: the add bid form component
+        """
+        cmp = dbc.Card([
+            dbc.CardBody([
+                dbc.Row([
+                    dbc.Col([
+                        self.lable("Bidder", for_input="bidder"),
+                        self.input_text(
+                            id="bidder",
+                            placeholder="Saeed",
+                            is_diable=True
+                        )
+                    ], width=3),
+                    dbc.Col([
+                        self.lable("Job Title", for_input="job_title"),
+                        self.input_text(
+                            id="job_title",
+                            placeholder="Add the job title",
+                            is_diable=False
+                        )
+                    ], width=9)
+                ]),
+                dbc.Row([
+                    dbc.Col([
+                        self.lable("Date", for_input="datepicker"),
+                        self.input_text(
+                            id="datepicker",
+                            placeholder="Add the job title",
+                            is_diable=False
+                        )
+                    ], width=6)
+                ])
+            ])
+        ])
+        return cmp
--- pages/add_bid.py
+++ pages/add_bid.py
@@ -1,6 +1,6 @@
 import dash_bootstrap_components as dbc
 from dash import dcc, html
-from dash.dependencies import Input, Output, State
+from components.components import Components
 
 class AddBid:
     def __init__(self):
@@ -10,7 +10,7 @@ class AddBid:
         :param: None
         :return: None
         """
-        pass
+        self.cmp = Components()
 
     def layout(self):
         """
@@ -20,22 +20,7 @@ class AddBid:
         :return: the layout of the home page
         """
         layout = dbc.Container([
-            html.H1("Welcome to My Dash App", className="mt-5 text-center"),
-            dbc.Row([
-                dbc.Col([
-                    html.Div("Name:", className="mb-2"),
-                    dcc.Input(id='name-input', type='text', placeholder='Enter your name', className="form-control mb-3")
-                ], width=6),
-                dbc.Col([
-                    html.Div("Age:", className="mb-2"),
-                    dcc.Input(id='age-input', type='number', placeholder='Enter your age', className="form-control mb-3")
-                ], width=3),
-                dbc.Col([
-                    html.Div("Salary:", className="mb-2"),
-                    dcc.Input(id='salary-input', type='number', placeholder='Enter your salary', className="form-control mb-3")
-                ], width=3)
-            ]),
-            dbc.Button("Submit", id='submit-button', n_clicks=0, color="primary", className="mt-3"),
-            html.Div(id='form-output', className="mt-3")
+            self.cmp.page_title("Add New Bid", bootstrap="mt-3 mb-3 text-center"),
+            self.cmp.add_bid_form()
         ])
         return layout
